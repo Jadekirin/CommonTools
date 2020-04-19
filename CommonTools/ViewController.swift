6cdea6006793576d22fa6d463c586eb68b411620
maweilong <908686451@qq.com>
2020-04-19 11:42:35 +0800

    首次提交

--- CommonTools/ViewController.swift
+++ CommonTools/ViewController.swift
@@ -15,6 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func DataCache(_ sender: Any) {
+        let mapData = "vsvffddvaaf"
+        
+        let mapFile = FilePathUtils.setupFilePath(directory: .documents, name: "mapData")
+        let result =  FileUtils.writeFile(content: mapData.data(using: String.Encoding(rawValue: String.Encoding.utf8.rawValue))!, filePath: mapFile)
+        print("保存数据结果---\(result)")
+    }
+    
+    @IBAction func DataRead(_ sender: Any) {
+        let path = FilePathUtils.setupFilePath(directory: .documents, name: "mapData")
+         let readData : Data? = FileUtils.readFile(filePath: path)
+         if readData == nil {
+             return
+         }
+         let readString =  String.init(data: readData ?? Data.init(), encoding:.utf8)
+        print("读取数据成功----\(readString)")
+    }
 }
 
